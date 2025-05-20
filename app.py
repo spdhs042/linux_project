1abafe7a2196edfe6cc3f605d0b964d72a630e19
spdhs042 <jenny050203@ewha.ac.kr>
2025-05-21 02:08:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -83,3 +83,6 @@ def result():
                            slides=slides,
                            answers=answers,
                            score=score)  # 결과 페이지 렌더링
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
