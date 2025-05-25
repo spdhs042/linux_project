2b620564c578549d199d7f67d74a85633e84a9fc
yujinson24 <sonyj0709@naver.com>
2025-05-26 00:12:31 +0900

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+echo "🔧 [1/4]ngrok 실행중 ..."
+snap install ngrok
+echo "🔐 [2/4] ngrok 인증 토큰 등록 중..."
+ngrok config add-authtoken 2xSBFg6HICtK0Ghvpmt6T0zkA4Y_6DkrBYZ8MgubVEmeBpPom
+
+echo "✅ [3/4] run.sh 실행 권한 부여 중..."
+chmod +x run.sh
+
+echo "🎉 초기 설정 완료! 이제 다음 명령어로 실행하세요:"
+echo "./run.sh"
